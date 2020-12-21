c0753e3f3bfe909dc1818758e55c8aeef97d8dc9
Sabina Schmitz <sschmitz@Sabinas-Mac.local>
2020-12-21 21:25:49 +0100

    new training texts

--- train.py
+++ train.py
@@ -15,11 +15,17 @@ trainer.train([
 'I want to learn more about discrimination',
 'Please tell me more',
 'What is discrimination',
-'Discrimination is..',
+'There are different kinds of discrimination. Gender, age, race, sexuality, religious belief. Which one would you like to talk about?',
 'How do I approach my friend about bias',
 'Think about...',
 'Okay Thanks',
 'No Problem! Have a Good Day!'
+])
+
+trainer.train([
+    'I would like to speak about racism',
+    'Ok, let us talk about racism. Racism is not only prevelant amongst radical people, but also with people who do not consider themselves racist. What many minority groups fight against, is the superiority ethos that predominantly white people live by.',
+
 ])
 
 name = input("What's your name?: ")
